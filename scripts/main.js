fda7cebfee8213f1e6471e5afb4b618d586c5045
Chaotix <westberginc1@gmail.com>
2025-09-14 19:01:34 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
+let editorArea = document.querySelector("#editorArea");
+let cLibrary = `[
+    {
+        "code": "while(%^0^%)",
+        "name": "while",
+        "type": "statement"
+    }
+]`;
 
+
+function addCode(c){
+    JSON.parse(cLibrary).forEach(element => {
+        if(element.name === c){
+            editorArea.textContent += element.code;
+
+            if(element.type == "statement"){
+                editorArea.textContent += "\n{\n}"
+            }
+        }
+    });
+};
